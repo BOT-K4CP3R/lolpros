e5c73b5a253dcb1e29f798703da4098e530cbffd
Kacper <Kontakt.n4rmoo@gmail.com>
2024-11-02 01:57:09 +0100

    Zaktualizowano wersję pakietu

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const { searchProfile } = require('./api/searchProfile');
 const { getGameDetails } = require('./api/lolGame');
+const { getLadder } = require('./api/ladder');
 
 module.exports = {
   searchProfile,
   getGameDetails,
+  getLadder,
 };
